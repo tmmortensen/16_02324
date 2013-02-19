66d4a383882530a602f14b15c94e925aa2c6504b
tmmortensen <thomas.martin.mortensen@gmail.com>
2013-02-19 17:42:47 +0100

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 
 public class Main {
 
+<<<<<<< HEAD
 	public static void main(String[] args) {
 
 		System.out.println("Test");
 
+=======
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("mere hej - Christin");
+>>>>>>> Test
 	}
 
 }
