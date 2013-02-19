30ad118a55195f81f504560dba007d72baa8d7ab
khannoori <khan_noori@msn.com>
2013-02-19 16:33:21 +0100

    Test

--- Hej.java
+++ Hej.java
@@ -6,12 +6,20 @@ public class Hej {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("Test");
 		System.out.println("Christiiiin tester også!")
 		System.out.println("Magnus tester også!")
 		System.out.println("Test fra Thomas")
+
 		System.out.println("Test fra Emil");
+
+
+		System.out.println("Test1");
+		System.out.println("Christiiiin tester ogs�!")
+
+
+
 	}
 
 }
